942c865a13ff3210356eb76eec489ddf84489d2e
Robert Thurnher <r.thurnher@gmail.com>
2011-09-03 02:26:55 +0200

    Fix tests.

--- src/test/scala/spec.scala
+++ src/test/scala/spec.scala
@@ -1,4 +1,4 @@
-import com.robert42.jvmng.{MongoConfig, MainDoc, MainDocs}
+import com.robert42.todosng.{MongoConfig, Todo, Todos}
 import org.junit.Test
 import com.codahale.simplespec.Spec
 import com.foursquare.rogue.Rogue._
@@ -9,24 +9,25 @@ class AppSpec extends Spec {
   class `A MongoDB document` {
     @Test def `can be CRUDed.` = {
       // Creation.
-      val json = """{"name": "md1", "cnt": 123, "isNew": true}"""
-      MainDocs create json
+      val json = """{"text": "Something.", "order": 1, "done": false}"""
+      Todos create json
 
       // Querying.
-      val query = MainDoc where (_.name eqs "md1") get
-      val md    = query.get
-      md.name.is must equalTo("md1")
-      md.cnt.is must equalTo(123)
+      val query = Todo where (_.text eqs "Something.") get
+      val todo  = query.get
+      todo.text.is   must equalTo("Something.")
+      todo.order.is  must equalTo(1)
+      todo.done.is   must equalTo(false)
 
       // Updating.
       val jsonForUpdate =
-        """{"name":"md2","_id":"%s","cnt":12,"isNew":false}"""
-          .format(md.id)
-      val updatedJson = MainDocs.update(jsonForUpdate)
+        """{"order":2,"_id":"%s","text":"Something else.","done":true}"""
+          .format(todo.id)
+      val updatedJson = Todos.update(jsonForUpdate)
       updatedJson must equalTo(jsonForUpdate)
 
       // Deletion.
-      MainDocs remove md.id.toString
+      Todos remove todo.id.toString
     }
   }
 }
